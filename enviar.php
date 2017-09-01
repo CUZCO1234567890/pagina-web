8f741f5eba7daf1a9e3656a2c0425e5d4c6652e8
cuzco1234567890 <cuzcoerick99@gamil.com>
2017-09-01 16:20:27 -0600

    hola ahi esta el proyecto html terminado

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$destino= "consulta@jygproyectosweb.com";
-	$nombre= $_post["nombre"];
-	$correo= $_post["correo"];
-	$telefono= $_post["telefono"];
-	$mensaje= $_post["mensaje"];
-	$contenido= "Nombre: ". $nombre ."\nCorreo: " .$correo . "\nTelefono: ". $telefono . "\nMensaje: " . $mensaje;mail($destino, "Contacto", $conenido);
-	header("location:graias.html")
-?>
